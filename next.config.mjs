97c12acf39a6f85bdd680ddcf5abbd0446053b96
Bryant Mejia <bmejia220@gmail.com>
2025-01-24 11:20:02 -0800

    trim deps

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 import withBundleAnalyzer from '@next/bundle-analyzer';
 import withNextIntl from 'next-intl/plugin';
-import CopyWebpackPlugin from 'copy-webpack-plugin';
-import path from 'path';
-import fs from 'fs'
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const withNextIntlConfig = withNextIntl('./src/lib/i18n.ts');
 
@@ -14,35 +10,5 @@ const bundleAnalyzer = withBundleAnalyzer({
 
 /** @type {import('next').NextConfig} */
 export default bundleAnalyzer(
-  withNextIntlConfig({
-    webpack(config, { isServer }) {
-      
-      // This is necessary because we only want to apply the plugin on the client side
-      if (!isServer) {
-        config.plugins.push(
-          new CopyWebpackPlugin({
-            patterns: [
-              {
-                from: path.resolve('node_modules/@audio-samples/piano-pedals/audio/'),
-                to: path.resolve(__dirname, 'public/samples/'), 
-              },
-              {
-                from: path.resolve('node_modules/@audio-samples/piano-velocity6/audio/'), // Path to source files in node_modules
-                to ({ absoluteFilename }) { return path.resolve(__dirname, 'public/samples/', absoluteFilename.split('/').pop().replace('#', 's')); },
-              },
-              {
-                from: path.resolve('node_modules/@audio-samples/piano-release/audio/'),
-                to: path.resolve(__dirname, 'public/samples/'), 
-              },
-              {
-                from: path.resolve('node_modules/@audio-samples/piano-harmonics/audio/'), 
-                to ({ absoluteFilename }) { return path.resolve(__dirname, 'public/samples/', absoluteFilename.split('/').pop().replace('#', 's')); }, 
-              },
-            ],
-          })
-        );
-      }
-      return config;
-    }
-  })
+  withNextIntlConfig()
 );
